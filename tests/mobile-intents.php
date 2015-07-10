ecd71ef1223aa135e1fa375acc4e9a53a9d8bfa0
courtaks <courtaks@yahoo.com>
2015-07-10 23:27:27 +0200

    fdasdf

--- tests/mobile-intents.php
+++ tests/mobile-intents.php
@@ -11,6 +11,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 
 <!-- Latest compiled and minified JavaScript -->
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
 </head>
