dff6c8dc444d9a00e528502754ed4b9c4cd6f98d
courtaks <courtaks@yahoo.com>
2015-07-21 06:43:33 +0200

    updated pages

--- site/resources/views/main-site/left/Pages/cv2.blade.php
+++ site/resources/views/main-site/left/Pages/cv2.blade.php
@@ -170,7 +170,7 @@
 
 
 
-@include(get_location("pages.cv_header"))
+@include(get_location("Pages.cv_header"))
 
 
 
@@ -184,12 +184,12 @@
 
 
 
-    @include(get_location("pages.cv-inc.cv_brief"))
-    @include(get_location("pages.cv-inc.cv_summery"))
-    @include(get_location("pages.cv-inc.cv_personal_info"))
-    @include(get_location("pages.cv-inc.cv_web_development_skills"))
-    @include(get_location("pages.cv-inc.cv_programing"))
-    @include(get_location("pages.cv-inc.cv_other_tech"))
+    @include(get_location("Pages.cv-inc.cv_brief"))
+    @include(get_location("Pages.cv-inc.cv_summery"))
+    @include(get_location("Pages.cv-inc.cv_personal_info"))
+    @include(get_location("Pages.cv-inc.cv_web_development_skills"))
+    @include(get_location("Pages.cv-inc.cv_programing"))
+    @include(get_location("Pages.cv-inc.cv_other_tech"))
 
 
 
