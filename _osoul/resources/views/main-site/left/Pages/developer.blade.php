f0ec29dfe2b1bf87d426e24ab3cb38e914b88318
courtaks <courtaks@yahoo.com>
2015-07-26 07:24:34 +0200

    renaming & replacing files

--- _osoul/resources/views/main-site/left/Pages/developer.blade.php
+++
@@ -1,7 +0,0 @@
-
-<h1>Welcome! {{sp(20)}} <code>I Am the Developer</code></h1>
-<h3> this is ---( Ahmed Badawy El-Said - أحمد بدوى السيد )--- the developer of the website</h3>
-
-
-<p>this is my <a href="http://ahmed-badawy.com" style='font-size:1.4em'>Personal Website</a></p>
-<p>this is my <a href="http://ahmed-badawy.com/site/cv" style='font-size:1.4em'>C.V</a></p>
