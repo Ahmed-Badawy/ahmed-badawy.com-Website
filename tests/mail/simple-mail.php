8254ac9a8fb0c67a7086b8bf9f564338bfa1c4a8
Ahmed Badawy <admin@ahmed-badawy.com>
2015-11-28 10:49:15 +0200

    ok

---
+++ tests/mail/simple-mail.php
@@ -0,0 +1,17 @@
+<?php
+
+	// The message
+	$message = "Line 1\r\nLine 2\r\nLine 3";
+
+	// In case any of our lines are larger than 70 characters, we should use wordwrap()
+	$message = wordwrap($message, 70, "\r\n");
+
+	// Send
+	$status = mail('ahmed.badawy.elsaid@gmail.com', 'Testing Send From Website', $message);
+
+	var_export($status);
+
+
+
+	
+?>
