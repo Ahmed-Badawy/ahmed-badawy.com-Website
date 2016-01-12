2623d1a797e0586a72fa83d73758d113b8dff04a
Ahmed Badawy <admin@ahmed-badawy.com>
2016-01-12 09:36:06 +0200

    ok

--- projects/php-file-generator/file-generator.php
+++ projects/php-file-generator/file-generator.php
@@ -6,6 +6,16 @@
 		fclose($myfile);
 		echo "<h4 class='alert alert-success'>File Was Created !</h4>";
 	}
+
+	if(isset($_POST['file-upload-submit'])){
+		die('file_upload_submit');
+		$myfile = fopen($_POST['file_name'], "w") or die("Unable to open file!");
+		$txt = $_POST['file_contents'];
+		fwrite($myfile, $txt);
+		fclose($myfile);
+		echo "<h4 class='alert alert-success'>File Was Created !</h4>";
+	}
+
 ?> 
 
 
@@ -36,6 +46,18 @@
 	<button type='reset' class='btn btn-danger col-sm-6 btn-lg'>Reset</button>
 	<button type='submit' name='file-generator-submit' class='btn btn-primary col-sm-6 btn-lg'>Submit</button>
 </form>
+
+
+
+<form action="#" method='post' enctype="multipart/form-data">
+	<label>File Name</label>
+	<input type='text' name='file_name' class='form-control' value='orders.php'>
+	<label>File</label>
+	<input type='file' name='uploaded_file' class='form-control'>
+	<button type='reset' class='btn btn-danger col-sm-6 btn-lg'>Reset</button>
+	<button type='submit' name='file-upload-submit' class='btn btn-primary col-sm-6 btn-lg'>Submit</button>
+</form>
+
 
 
 
